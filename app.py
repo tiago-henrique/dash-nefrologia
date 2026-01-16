5b90ae761cea6dd6c2d40443958f60ae15417ab3
Tiago Henrique <tiago_henrique@hotmail.com>
2026-01-16 10:44:50 -0300

    Enable file upload for CSV in app.py

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ st.set_page_config(
 )
 
 st.title("Análise de Sobrevida – Transplante Renal")
-uploaded_file = "https://imunogenetica.famerp.br/nefrologia/indicadores.csv";
+#uploaded_file = "https://imunogenetica.famerp.br/nefrologia/indicadores.csv";
 
-#uploaded_file = st.file_uploader(
-#    "Envie o arquivo CSV",
-#    type="csv"
-#)
+uploaded_file = st.file_uploader(
+    "Envie o arquivo CSV",
+    type="csv"
+)
 
 if uploaded_file:
 
